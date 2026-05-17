1964a334b675104f0b198de5428d69e12e0d1fe7
maxeozakh <maxeozakh@gmail.com>
2026-05-17 10:13:45 +0000

    now it is

--- hello_cuda.cpp
+++ hello_cuda.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cude_runtime.h>
+#include <cuda_runtime.h>
 
 void vecAdd(float *A_h, float *B_h, float *C_h, int n)
 {
@@ -7,16 +7,16 @@ void vecAdd(float *A_h, float *B_h, float *C_h, int n)
 	int size = n * sizeof(float);
 	float *A_d, *B_d, *C_d;
 
-	cudaMalloc((void **), &A_d, size);
-	cudaMalloc((void **), &B_d, size);
-	cudaMalloc((void **), &C_d, size);
+	cudaMalloc((void **)&A_d, size);
+	cudaMalloc((void **)&B_d, size);
+	cudaMalloc((void **)&C_d, size);
 
 	cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
 	cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
 
 	// addition here
 
-	cudaMemcpy(C_h, C_d, size, cudaMemcpyHostToHost);
+	cudaMemcpy(C_h, C_d, size, cudaMemcpyDeviceToHost);
 
 	cudaFree(A_d);
 	cudaFree(B_d);
@@ -27,5 +27,5 @@ int main()
 {
 	float A_h{}, B_h{}, C_h{};
 
-	vecAdd(&A_h, &B_h, &C_h, 4);
+	vecAdd(&A_h, &B_h, &C_h, 1);
 };
