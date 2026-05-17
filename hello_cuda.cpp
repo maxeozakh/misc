2d82883d152a8072e51371f2f5dc35cf38eb7b41
Maksim Zakharov <maxeozakh@gmail.com>
2026-05-17 11:51:51 +0200

    will it run on gpu?

---
+++ hello_cuda.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cude_runtime.h>
+
+void vecAdd(float *A_h, float *B_h, float *C_h, int n)
+{
+	std::cout << "hey" << "\n";
+	int size = n * sizeof(float);
+	float *A_d, *B_d, *C_d;
+
+	cudaMalloc((void **), &A_d, size);
+	cudaMalloc((void **), &B_d, size);
+	cudaMalloc((void **), &C_d, size);
+
+	cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
+
+	// addition here
+
+	cudaMemcpy(C_h, C_d, size, cudaMemcpyHostToHost);
+
+	cudaFree(A_d);
+	cudaFree(B_d);
+	cudaFree(C_d);
+};
+
+int main()
+{
+	float A_h{}, B_h{}, C_h{};
+
+	vecAdd(&A_h, &B_h, &C_h, 4);
+};
